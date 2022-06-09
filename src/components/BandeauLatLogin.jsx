ee6497d44d6895e053fbf219243aa84eee2f51b5
Léo Mullier <leo.mullier@etu.utc.fr>
2022-06-09 20:27:04 +0200

    Premier transfert de données json réussi

--- src/components/BandeauLatLogin.jsx
+++ src/components/BandeauLatLogin.jsx
@@ -4,108 +4,114 @@
 //                                                                                                      //
 // Nom du fichier : BandeauLatLogin.jsx                                                                 //
 // Description : Script JS pour afficher le bandeau latéral lorsqu'on est sur la phase de login         //
-// Date de dernière mise à jour : 08/06/2022                                                            //
+// Date de dernière mise à jour : 09/06/2022                                                            //
 // ==================================================================================================== //
 
 
 // Importations
 import React from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 
 
-
+// Fonction principale
 const BandeauLatLogin = () => {
-  
-  const [formValue, setformValue] = React.useState({
-    loginTmp: '',
-    mdpTmp: ''
-  });
+	// Définition de toutes les variables états
+	const [etatTmp, setetatTmp] = React.useState({
+		loginTmp: '',
+		mdpTmp: ''
+	});
 
-  
-    const handleSubmit = async() => {
-		// store the states in the form data
-		const loginFormData = new FormData();
-		loginFormData.append("login", formValue.loginTmp)
-		loginFormData.append("mdp", formValue.mdpTmp)
-	  alert("hey")
+
+	// Fonction de soumission du formulaire
+	const actionValiderFormulaire = async() => {
+		// Préparation des données Json
+		const donneesJson = new FormData();
+		donneesJson.append("login", etatTmp.loginTmp)
+		donneesJson.append("mdp", etatTmp.mdpTmp)
+		
+		
+		// Envoi de la requête vers serveur Rest
 		try {
-		  // make axios post request
-		  const response = await axios({
-			method: "post",
-			url: "http://localhost:8080/user/test2",
-			data: loginFormData,
-			headers: { "Content-Type": "application/json" },
-		  });
+			const response = await fetch('http://localhost:8080/user/test2',
+			{
+				method: 'POST',
+				headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+				body:JSON.stringify({login: etatTmp.loginTmp, mdp: etatTmp.mdpTmp})
+			});
+			const result = await response.json();
+
+			// Action à la réception de la réponse
+			alert("test" + result)
+			console.log(result);
+
 		} catch(error) {
-		  console.log(error)
+			// Gestion des exceptions
+			alert("(!) Désolé, nous n'avons pu faire aboutir l'envoi de la requête ou la réception de sa réponse. Veuillez réessayer ultérieurement. Message d'erreur associé : " + error)
 		}
-	  }
-  
+	}
 
-  const handleChange = (event) => {
-    setformValue({
-      ...formValue,
-      [event.target.name]: event.target.value
-    });
-  }
 
-  return (
-	<div className="bandeaulatlogin">
-				<aside id="panneau">
-					<div class="panneau_corps">
+	// Fonctions de mise à jour des états
+	const actionChangerEtatLogin = (event) => {
+		setetatTmp({
+			...etatTmp,
+			loginTmp: event.target.value
+		});
+	}
+
+	const actionChangerEtatMdp = (event) => {
+		setetatTmp({
+			...etatTmp,
+			mdpTmp: event.target.value
+		});
+	}
+
+
+	// Fonction de render
+	return (
+		<div className="bandeaulatlogin">
+			<aside id="panneau">
+				<div class="panneau_corps">
 					<center>
-							{/* Image aléatoire */}
-							<img class="panneau_image" src="https://picsum.photos/500"/>
+					{/* Image aléatoire */}
+					<img class="panneau_image" src="https://picsum.photos/500"/>
 
-							{/* Formulaire d'authentification */}
-    <form onSubmit={handleSubmit}>
-		<label>Email :</label>
-		<br />
-		<input type="text" class="login_champ" placeholder="Tapez ici..."  required onChange={handleChange}/>
-		<br />
-		<br />
-		<label>Mot de passe :</label>
-		<br />
-		<input type="password" class="login_champ" placeholder="Tapez ici..."  required onChange={handleChange}/>
-		<br />
-		<br />
-		<input type="checkbox" class="login_check" checked="checked"/> Se souvenir de moi
-		<br />
-		<input class="login_valider" type="submit" value="Se connecter"/>
-      {/* <p>Login Form</p>
-      <input
-        type="email"
-        name="email"
-        placeholder="enter an email"
-        value={formValue.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="enter a password"
-        value={formValue.password}
-        onChange={handleChange}
-      />
-      <button
-        type="submit"
-      >
-        Login
-      </button> */}
-    </form>
-	</center>
-					</div>
-					
-					{/* Pied de page */}
-					<footer class="panneau_pied">
-						<p>
-							&lt;/&gt; Développé pour SR03 en P2022 par
-							<br />Bastian Cosson, Léo Mullier, Cédric Martinet
-						</p>
-					</footer>
-				</aside>
-			</div>
-  )
+					{/* Formulaire d'authentification */}
+					<form onSubmit={actionValiderFormulaire}>
+						<label>Email :</label>
+						<br />
+						<input type="text" class="login_champ" placeholder="Tapez ici..."  required onChange={actionChangerEtatLogin}/>
+						<br />
+						<br />
+						<label>Mot de passe :</label>
+						<br />
+						<input type="password" class="login_champ" placeholder="Tapez ici..."  required onChange={actionChangerEtatMdp}/>
+						<br />
+						<br />
+						<input type="checkbox" class="login_check" checked="checked"/> Se souvenir de moi
+						<br />
+						<input class="login_valider" type="submit" value="Se connecter"/>
+						</form>
+					</center>
+				</div>
+
+				{/* Pied de page */}
+				<footer class="panneau_pied">
+					<p>
+						&lt;/&gt; Développé pour SR03 en P2022 par
+						<br />Bastian Cosson, Léo Mullier, Cédric Martinet
+					</p>
+				</footer>
+			</aside>
+		</div>
+	)
 };
 
+
+// Export vers le reste du projet
 export default BandeauLatLogin;
+
+
+
+
